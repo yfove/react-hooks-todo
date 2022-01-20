fbe2641ebe665d850ebb689497eeb643eb77c4aa
curtis chat <curtis.ch.chat@gmail.com>
2022-01-19 23:22:07 -0700

    delete function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'
 
 function App() {
   const [todos, setTodos] = useState([])
   const [todo, setTodo] = useState("")
- 
+
   function handleSubmit(e) {
-    e.preventDefault() //page will not refresh when submit
+    e.preventDefault()
+
     const newTodo = {
       id: new Date().getTime(),
       text: todo,
       completed: false,
     }
+    
+    setTodos([...todos].concat(newTodo))
+    setTodo("")
+  }
 
-    setTodos([...todos].concat(newTodo)) // spread operator copies data to new array
-    setTodo("") //restore default value of empty string
+  function deleteTodo(id) {
+    const updatedTodos = [...todo].filter((todo) => todo.id !== id)
+    setTodos(updatedTodos)
   }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo}/>
+        <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} />
         <button type="submit">Add Todo</button>
       </form>
-      {todos.map((todo) => <div>{todo.text}</div>)}
+      {todos.map((todo) => <div key={todo.id}>
+        <div>{todo.text}</div>
+        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+      </div>)}
     </div>
-  );
+  )
 }
 
 
-export default App;
+export default App
