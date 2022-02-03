7756cbe1944def1de2e7c4c448885658331507f7
curtis chat <curtis.ch.chat@gmail.com>
2022-02-03 14:39:19 -0700

    toggle complete function

--- src/App.js
+++ src/App.js
@@ -1,40 +1,61 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     const newTodo = {
       id: new Date().getTime(),
       text: todo,
       completed: false,
-    }
-    
-    setTodos([...todos].concat(newTodo))
-    setTodo("")
+    };
+
+    setTodos([...todos].concat(newTodo));
+    setTodo("");
   }
 
   function deleteTodo(id) {
-    const updatedTodos = [...todo].filter((todo) => todo.id !== id)
-    setTodos(updatedTodos)
+    const updatedTodos = [...todo].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
+
+  function toggleComplete(id) {
+    const updatedTodos = [...todo].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
   }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} />
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
         <button type="submit">Add Todo</button>
       </form>
-      {todos.map((todo) => <div key={todo.id}>
-        <div>{todo.text}</div>
-        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-      </div>)}
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <div>{todo.text}</div>
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          <input
+            type="checkbox"
+            onChange={() => toggleComplete(todo.id)}
+            checked={todo.completed}
+          />
+          <button>Edit Todo</button>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
